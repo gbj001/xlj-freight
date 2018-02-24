f1e6ccc91af2c9223bd2bbb34edf56af8c9f5feb
gbj001 <gbj001@126.com>
2018-02-24 10:22:33 +0800

    fix interface

--- src/main/java/cn/xlj/modules/freight/controller/CalculateFreightController.java
+++ src/main/java/cn/xlj/modules/freight/controller/CalculateFreightController.java
@@ -1,20 +1,14 @@
 package cn.xlj.modules.freight.controller;
 
-import cn.xlj.common.exception.RRException;
-import cn.xlj.common.exception.RRExceptionHandler;
-import cn.xlj.common.utils.PageUtils;
 import cn.xlj.common.utils.R;
 import cn.xlj.modules.freight.dto.OrderDto;
-import cn.xlj.modules.freight.entity.OrderDetailEntity;
 import cn.xlj.modules.freight.entity.OrderEntity;
 import cn.xlj.modules.freight.service.OrderService;
 import org.apache.commons.lang.StringUtils;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 
@@ -35,29 +29,32 @@ public class CalculateFreightController {
      */
     @RequestMapping(value = "/calculate/freight-fee", method = RequestMethod.POST)
     @ResponseBody
-    public R calculateFreightFee(@RequestBody OrderDto orderDto) throws RRExceptionHandler {
+    public R calculateFreightFee(@RequestBody OrderDto orderDto) {
         Map map = new HashMap();
-        //1、接受数据插入运费表
-        freightService.save(orderDto);
+        try {
+            //1、接受数据插入运费表
+            freightService.save(orderDto);
 
-        //2、根据规则计算出运费更新运费表(暂时随机)
-        String fee = freightService.calculateFee(orderDto);
-        if (StringUtils.isNotEmpty(fee)) {
-            OrderEntity freightEntity = freightService.queryByRequestId(orderDto.getRequestId());
-            freightEntity.setExpectFee(Double.parseDouble(fee));
-            freightService.update(freightEntity);
-            map.put("requestId", orderDto.getRequestId());
-            map.put("message", "success");
-            map.put("successful", true);
-            map.put("code", "000");
-            map.put("freight_fee", fee);
-        }
-        else{
-            map.put("requestId", orderDto.getRequestId());
-            map.put("message", "请人工计算运费");
-            map.put("successful", false);
-            map.put("code", "400");
-            map.put("freight_fee", null);
+            //2、根据规则计算出运费更新运费表(暂时随机)
+            String fee = freightService.calculateFee(orderDto);
+            if (StringUtils.isNotEmpty(fee)) {
+                OrderEntity freightEntity = freightService.queryByRequestId(orderDto.getRequestId());
+                freightEntity.setExpectFee(Double.parseDouble(fee));
+                freightService.update(freightEntity);
+                map.put("requestId", orderDto.getRequestId());
+                map.put("message", "success");
+                map.put("successful", true);
+                map.put("code", "000");
+                map.put("freight_fee", fee);
+            } else {
+                map.put("requestId", orderDto.getRequestId());
+                map.put("message", "请人工计算运费");
+                map.put("successful", true);
+                map.put("code", "400");
+                map.put("freight_fee", null);
+            }
+        }catch(Exception e){
+            return R.error(500,"系统错误");
         }
 
         return R.ok(map);
